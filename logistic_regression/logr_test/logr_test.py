1575a74b315c490851a8eb3ee2937bb2d06869ff
zhoupengfei2 <zhoupengfei2@billionway.cn>
2020-03-27 13:43:53 +0800

    logr

--- logistic_regression/logr_test/logr_test.py
+++ logistic_regression/logr_test/logr_test.py
@@ -4,6 +4,8 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn.metrics
 
 
 def initial_data(n=100):
@@ -14,8 +16,8 @@ def initial_data(n=100):
 
     y_1 = np.array([1] * n).reshape(n, 1)
 
-    x1_0 = np.random.normal(0.6, 0.1, n).reshape(n, 1)
-    x2_0 = np.random.normal(0.3, 0.1, n).reshape(n, 1)
+    x1_0 = np.random.normal(1, 0.6, n).reshape(n, 1)
+    x2_0 = np.random.normal(1.5, 0.6, n).reshape(n, 1)
 
     y_0 = np.array([0] * n).reshape(n, 1)
 
@@ -24,7 +26,7 @@ def initial_data(n=100):
     x0 = np.array([1] * 2 * n).reshape(2 * n, 1)
     y = np.concatenate((y_1, y_0), axis=0)
 
-    x = np.concatenate((x0, x1, x2), axis=1)
+    x = np.concatenate((x1, x2), axis=1)
 
     plt.figure(0)
 
@@ -34,6 +36,55 @@ def initial_data(n=100):
     return x, y
 
 
+def confusion(label, label_p):
+    """混淆矩阵计算
+    """
+    # TP FP TN FN
+    tp = (label_p[label == 1] == 1).tolist().count(True)
+    fn = (label_p[label == 1] == 0).tolist().count(True)
+    tn = (label_p[label == 0] == 0).tolist().count(True)
+    fp = (label_p[label == 0] == 1).tolist().count(True)
+
+    c = np.array([[tp, tn], [fp, fn]])
+
+    c = pd.DataFrame(c, index=["T", "F"], columns=["P", "N"])
+
+    print("confusion detail")
+    print(c)
+    print("precision: ", tp / (tp + fp))
+    print("recall: ", tp / (tp + fn))
+
+
+def plot_best_fit(weights, intercept):
+    """在原始数据上划出决策边界
+    """
+    xx1 = np.arange(0, 3, 0.1)
+    xx2 = -(np.array([1.0]*len(xx1)) * intercept[0] + xx1 * weights[0][0]) / weights[0][1]
+    plt.plot(xx1, xx2)
+    plt.show()
+
+
+def roc_curve(y_true, y_score):
+    """roc 曲线
+    """
+    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_score, drop_intermediate=True)
+
+    plt.figure(1)
+    plt.scatter(fpr, tpr, marker="o", c="r")
+    plt.plot(fpr, tpr, c="b")
+    plt.title("ROC")
+    plt.show()
+
+    auc_area(fpr, tpr)
+
+
+def auc_area(x, y):
+    """auc 面积
+    """
+    auc = sklearn.metrics.auc(x, y)
+    print("AUC: ", auc)
+
+
 if __name__ == "__main__":
 
     # 生成初始数据
@@ -41,33 +92,22 @@ if __name__ == "__main__":
 
     m, n = indata.shape
 
-    indata = np.mat(indata)
-    label = np.mat(label)
+    model = LogisticRegression()
 
-    weights, his_weights = LogisticRegression(indata, label)
+    model.fit(indata, label)
 
     # 计算出来的权重
-    print(weights)
+    print("weights: ", model.coef_)
+    print("intercept: ", model.intercept_)
 
-    # 划出决策面 根据 1.0/(1+e^(-z))可知，当z>0时，即x0*w0+x1*w1+x2*w2>0 取1，反之取0
-    xx1 = np.arange(0, 3, 0.1)
-    xx2 = -(np.array([1.0]*len(xx1)) * weights[0][0] + xx1 * weights[0][1]) / weights[0][2]
-    plt.plot(xx1, xx2)
-    plt.show()
+    # 划决策线
+    plot_best_fit(model.coef_, model.intercept_)
 
-    # 参数的迭代收敛情况
-    plt.figure(1)
+    # 混淆矩阵
+    label_p = model.predict(indata)
+    confusion(label.transpose()[0], label_p)
 
-    plt.subplot(3, 1, 1)
+    # ROC 和 AUC
+    roc_curve(label, model.decision_function(indata))
 
-    plt.plot(his_weights[0])
-    plt.ylabel("x0")
 
-    plt.subplot(3, 1, 2)
-    plt.plot(his_weights[1])
-    plt.ylabel("x1")
-
-    plt.subplot(3, 1, 3)
-    plt.plot(his_weights[2])
-    plt.ylabel("x2")
-    plt.show()
